df99ab2b32504b66c34e91dc8839f07bb4fa174a
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-23 12:26:34 +0100

    feat: Added postgress

--- backend/check_data.sh
+++
@@ -1,8 +0,0 @@
-# Check if data is loaded
-if python -c "import sys; from users.models import CustomUser; sys.exit(0 if CustomUser.objects.exists() else 1)"; then
-    echo "Database is populated, skipping loaddata."
-else
-    echo "Database is empty, running loaddata."
-    python ./manage.py loaddata ./population/01_Users.json ./population/02_Products.json
-fi
-

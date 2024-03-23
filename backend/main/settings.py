453a7617d45856bad8602f4a22152686fbd098f7
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-23 11:42:26 +0100

    feat: Added postgress

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
+
+import dj_database_url
 import paypalrestsdk
 from dotenv import load_dotenv
 
@@ -21,6 +23,7 @@ load_dotenv()
 
 RUTA_BACKEND = os.getenv('RUTA_BACKEND')
 RUTA_FRONTEND = os.getenv('RUTA_FRONTEND')
+EXTERNAL_DATABASE = os.getenv('EXTERNAL_DATABASE')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -101,13 +104,17 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if EXTERNAL_DATABASE == 'True':
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL')) # postgres://shar3d_user:DUiiIGYBzjV7e1Yhel3NlRjXXHqlddea@dpg-cnvajqv79t8c73d5p000-a/shar3d
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
